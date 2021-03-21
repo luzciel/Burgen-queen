9afd41204b1f5b5d284b2aeb6f3f4def35bf4b11
erikaArango <enfermeraerikaarango@gmail.com>
2021-03-21 13:00:17 -0300

    botones desayuno y almuerzo

---
+++ src/views/OrdersViews.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Orders.css"
+import MenuButtons from '../components/orders/MenuButtons'
+//import orders from '../components/orders/OrdersInputs'
+const OrdersViews = () =>{
+    return (
+        <div>
+            <MenuButtons/>
+            
+        </div>
+
+    )
+}
+
+export default OrdersViews
