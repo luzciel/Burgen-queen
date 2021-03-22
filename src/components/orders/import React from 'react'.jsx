c5fcb1e346c07408b98ef246b35c504b5f9fece0
erikaArango <enfermeraerikaarango@gmail.com>
2021-03-22 13:07:25 -0300

    data menu desayuno

---
+++ src/components/orders/import React from 'react'.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import "./Orders.css"
+
+const BreakfastMenu = () =>{
+    
+            //console.log ('hola desayuno')
+        const [menu, setMenu] = React.useState([])
+
+            React.useEffect(() => {
+                //const data  = await fetch('https://luzciel.github.io/Burgen-queen/src/data/menu.json')
+                //console.log('useEffect')
+                getData()
+                console.log(menu)
+            }, [])
+
+            const getData = async () => {
+               const data  = await fetch('https://luzciel.github.io/Burgen-queen/src/data/menu.json')
+                const desayuno = await data.json()
+                const options = desayuno.desayuno
+                setMenu(desayuno)
+                
+
+
+            }
+   
+
+    return (
+   
+        <div>
+            {
+            menu.forEach(item =>(
+                    <li key={item.id}>{item.producto} - {item.precio}</li>
+                ))
+            }  
+        </div>
+    )
+
+}
+
+
+export default BreakfastMenu
+
+
