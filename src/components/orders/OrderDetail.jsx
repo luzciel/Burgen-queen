dcecece55a00404e28a176175867d6d26e0c92b9
Luzciel <luzcielmontesinos@gmail.com>
2021-03-24 21:30:56 -0300

    CSS de OrderDetail

--- src/components/orders/OrderDetail.jsx
+++ src/components/orders/OrderDetail.jsx
@@ -1,40 +1,46 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 // import LunchMenu from './LunchMenu';
 import LunchView from './LunchView';
 import SendKitchen from './SendKitchen';
-import {db} from '../../firebase'
+import { db } from '../../firebase'
+import './OrderDetail.css'
 
-const OrderDetail = ({cart, setCart}) => {
-  
-    //Sumar el total de los items
-    const productPrices = cart.map((item) => Math.floor(item.precio)); // Recorre el carrito y crea un nuevo array con los precios (NUMBER)
-    const grandTotal = productPrices.reduce((a, b) => a + b, 0);
+const OrderDetail = ({ cart, setCart }) => {
 
-    const getSubTotal = (subTotal) => {
-        console.log("HELLOOOOOOO", subTotal)
+  //Sumar el total de los items
+  const productPrices = cart.map((item) => Math.floor(item.precio)); // Recorre el carrito y crea un nuevo array con los precios (NUMBER)
+  const grandTotal = productPrices.reduce((a, b) => a + b, 0); // reduce, toma todos los elementos en un array, y los reduce en un solo valor.
+
+  const getSubTotal = (subTotal) => {
+    console.log("HELLOOOOOOO", subTotal)
+  }
+
+  //Funcion que detalla el contenido de la orden si esta tiene un lenght diferente a 0
+  const listLunchView = () => {
+    if (cart.length === 0) {
+      return <p>No hay productos en el carrito</p>;
+    } else {
+      return cart.map(((item, index) => <LunchView key={index + "menu"} item={item} cart={cart} setCart={setCart} sendSubTotal={getSubTotal} />))
     }
-    
-    //Funcion que detalla el contenido de la orden si esta tiene un lenght diferente a 0
-    const listLunchView = () => {
-        if(cart.length === 0) {
-            return <p>No hay productos en el carrito</p>;
-        } else {
-            return cart.map((item => <LunchView key={item.id} item={item} cart={cart} setCart={setCart} sendSubTotal={getSubTotal} />))
-        }
-    }
+  }
 
-    const addCollectionOrders = async (product) =>{
-        await db.collection('orders').doc().set(product)
-        console.log("NEW COLECTION")
-    }
+  const addCollectionOrders = async (product) => {
+    await db.collection('orders').doc().set(product)
+    console.log("NEW COLECTION")
+  }
 
-    return (
-        <div className='ordersDetail col-sm'>
-            <h4>Detalle de Orden</h4>
-            {listLunchView()}
-            <h1>Total {grandTotal} </h1>       
-            <SendKitchen/>
-        </div>
-    )
+  return (
+    <div className='orders-detail col-sm'>
+      <h4 className='orders-detail-title'>Detalles</h4>
+      <table class="table">
+        <thead>
+          <tr></tr>
+        </thead>
+      {listLunchView()}
+      </table>
+      <h1>Total {grandTotal} </h1>
+      <SendKitchen />
+    </div>
+  )
 }
 export default OrderDetail;
--- src/components/orders/SendKitchen.jsx
+++ src/components/orders/SendKitchen.jsx
@@ -1,12 +1,18 @@
 import React, {useState} from 'react';
+import {db} from '../../firebase'
 
 const SendKitchen = () => {
+
+    const addCollectionOrders = async (product) =>{
+        await db.collection('orders').doc().set(product)
+        console.log("NEW COLECTION")
+    }
 
     return (
 
     <div>
-        <button>Enviar a cocina</button>
-        <button>Cancelar Pedido</button>
+        <button type="button" class="btn btn-primary btn-sm">Enviar a cocina</button>
+        <button type="button" class="btn btn-primary btn-sm">Cancelar Pedido</button>
     </div>
     )
 
