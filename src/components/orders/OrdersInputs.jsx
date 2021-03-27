9133ed65337172e2dde87d96d87b6e3d6367d434
erikaArango <enfermeraerikaarango@gmail.com>
2021-03-27 13:22:25 -0300

    css orders

--- src/components/orders/OrdersInputs.jsx
+++ src/components/orders/OrdersInputs.jsx
@@ -4,15 +4,12 @@ import MenuButtons from './MenuButtons'
 // import NumberTables from '../tables/NumberTable'
 
 const OrdersInputs = (props) =>{
-    // console.log("hola", props)
     return (
         <div className="inputsContainer">
             <input type="text" className="clientName" placeholder="Nombre Cliente"/>
             <input type="text" className="waiterName" placeholder="Garzón" />
             {/* <input type="text" className="tableNumber" placeholder="Mesa" value={`Mesa ${props.match.params.numTable}`} />
-            <hr className="hrOrders"></hr> */}
-        
-            
+            <hr className="hrOrders"></hr> */}            
         </div>
 
     )
