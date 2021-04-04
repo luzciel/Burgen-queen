763c54b6f68368a1248b1c4956f68eb02b8b4c52
erikaArango <enfermeraerikaarango@gmail.com>
2021-04-04 19:18:48 -0400

    estilos botones mesas

--- src/components/tables/Tables.jsx
+++ src/components/tables/Tables.jsx
@@ -7,7 +7,7 @@ const Tables = () => {
     return (
         <div className='container-views-tables'>
            <div className='text-tables'>
-            <h1>Elige una mesa</h1>
+            <h1 class ="textH1" >Elige una mesa</h1>
             <Link to='/cocina'><button type="button" class="btn btn-primary btn-lg to-go">Para Llevar</button></Link>
            </div>
             <div className='container-tables'>
